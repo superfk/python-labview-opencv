9cde07df43a8f130ca7d478e08637b1a7e9e2750
shawn.hsiao <jungenhsiao@gmail.com>
2020-03-18 01:09:10 +0800

    change qrcode library to pyzbar

--- py_qrcode_reader.py
+++ py_qrcode_reader.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from pyzbar import pyzbar
 
 # Display barcode and QR code location
 def display(im, bbox):
@@ -30,3 +31,38 @@ def detect_qr_code(source_path, output_path):
         found = 0
         decode_data = ""
     return (found, decode_data)
+
+def detect_zbar(source_path, output_path):
+    try:
+        # load the input image
+        image = cv2.imread(source_path)
+        # find the barcodes in the image and decode each of the barcodes
+        barcodes = pyzbar.decode(image)
+        # loop over the detected barcodes
+        for barcode in barcodes:
+            # extract the bounding box location of the barcode and draw the
+            # bounding box surrounding the barcode on the image
+            (x, y, w, h) = barcode.rect
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            # the barcode data is a bytes object so if we want to draw it on
+            # our output image we need to convert it to a string first
+            barcodeData = barcode.data.decode("utf-8")
+            barcodeType = barcode.type
+            # draw the barcode data and barcode type on the image
+            text = "{} ({})".format(barcodeData, barcodeType)
+            cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 0, 255), 2)
+            # print the barcode type and data to the terminal
+            print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
+        # show the output image
+        cv2.imwrite(output_path,image)
+        found = 1
+        barcodeData = barcodeData
+    except:
+        found = 0
+        barcodeData = ''
+    return (found,barcodeData)
+
+if __name__ == "__main__":
+    ret = detect_zbar("meet.png", "decode.png")
+    print(ret)
